e71cf25fa446e1996ba848a3de63c03c735fc024
Miro We <39390507+Wyrda1@users.noreply.github.com>
2018-07-13 00:09:09 +0200

    moved logic inside setTimeout

--- src/providers/timedifference/timedifference.ts
+++ src/providers/timedifference/timedifference.ts
@@ -21,17 +21,15 @@ export class TimedifferenceProvider {
   public anzeigesekunde;
 
   runTimer(seconds) {
-    var ophase = moment("2018-10-08T08:00");
-    var ms = moment.duration(moment(ophase).diff(moment())).asSeconds();
-    this.anzeigemonat = moment.duration(ms, "seconds").months();
-    this.anzeigetag = moment.duration(ms, "seconds").days();
-    this.anzeigestunde = moment.duration(ms, "seconds").hours();
-    this.anzeigeminute = moment.duration(ms, "seconds").minutes();
-    this.anzeigesekunde = moment.duration(ms, "seconds").seconds();
-
-
     console.log("runTimer aufgerufen");
     setTimeout(() => {
+      var ophase = moment("2018-10-08T08:00");
+      var ms = moment.duration(moment(ophase).diff(moment())).asSeconds();
+      this.anzeigemonat = moment.duration(ms, "seconds").months();
+      this.anzeigetag = moment.duration(ms, "seconds").days();
+      this.anzeigestunde = moment.duration(ms, "seconds").hours();
+      this.anzeigeminute = moment.duration(ms, "seconds").minutes();
+      this.anzeigesekunde = moment.duration(ms, "seconds").seconds();
       seconds--;
       if (seconds>0) {
         this.runTimer(seconds);
