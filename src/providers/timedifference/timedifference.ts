ed72c91df220e258a20beeac1d49dfaf2023705d
Miro We <39390507+Wyrda1@users.noreply.github.com>
2018-09-27 11:35:20 +0200

    updated timedifferenceprovider

--- src/providers/timedifference/timedifference.ts
+++ src/providers/timedifference/timedifference.ts
@@ -24,9 +24,15 @@ export class TimedifferenceProvider {
 
   runTimer(page) {
     console.log("runTimer aufgerufen", page);
-    var ophase = moment("2018-10-08T08:00"); //time of the event
+    var ophase = moment("2018-10-08T08:00");
+    var kneipenbesuch = moment("2018-10-08T20:00");
+    var fruehstueck1 = moment("2018-10-08T08:00");
+    var fruestueck2 = moment("2018-10-08T08:00");
+    var fruestueck3 = moment("2018-10-08T08:00");
     setTimeout(() => {
-      var ms = moment.duration(moment(ophase).diff(moment())).asSeconds(); //time difference between current time and o-phase in seconds
+      var ophasetogo = moment.duration(moment(ophase).diff(moment())).asSeconds(); //time difference between current time and o-phase in seconds
+      var kneipenbesuchtogo = moment.duration(moment(kneipenbesuch).diff(moment())).asSeconds();
+      var ms = Math.min(ophasetogo, kneipenbesuchtogo);
       this.anzeigemonat = moment.duration(ms, "seconds").months(); //months of the time difference
       this.anzeigetag = moment.duration(ms, "seconds").days(); //days of the time difference and so on
       this.anzeigestunde = moment.duration(ms, "seconds").hours();
