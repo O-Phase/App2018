b00d4dee5f5924b1d7127bb3ad0caf74ce82774f
Miro We <39390507+Wyrda1@users.noreply.github.com>
2018-09-28 23:17:40 +0200

    first version of first time alert (doesnt work yet )

--- src/pages/news-feed/news-feed.ts
+++ src/pages/news-feed/news-feed.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { AlertController } from 'ionic-angular';
 import { TimedifferenceProvider } from '../../providers/timedifference/timedifference';
 
 /**
@@ -17,18 +19,39 @@ import { TimedifferenceProvider } from '../../providers/timedifference/timediffe
 })
 export class NewsFeedPage {
 
-  constructor(private timedifference: TimedifferenceProvider, public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private timedifference: TimedifferenceProvider, public navCtrl: NavController, private alertCtrl: AlertController, private storage: Storage, public navParams: NavParams) {
   }
 
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NewsFeedPage');
     this.timedifference.runTimer("NewsFeedPage");
+    /*this.storage.get("firstLogin").then((firstLoginLocal) => {
+      if (firstLoginLocal != true) {
+        this.FirstTimeAlert;
+        console.log("first Login?" + firstLoginLocal);
+      }
+      this.storage.set("firstLogin", true);
+    });*/
+  }
+
+  ionViewDidEnter() {
+    this.FirstTimeAlert;
   }
   
 
   openMap() {
     this.navCtrl.push('KartePage');
+  }
+
+  FirstTimeAlert() {
+    console.log("firsttimealert called");
+    let alert = this.alertCtrl.create({
+      title: 'Willkommen zur O-Phasen App 2018',
+      message: "<p>Beispieltext der sehr lang ist, die Ophase ist richtig cool habt Spaß trinkt viel Bier und eskaliert richtig. Lets gooooooooooooo!!</p>",
+      buttons: ['Ok']
+    });
+    alert.present();
   }
 
 
