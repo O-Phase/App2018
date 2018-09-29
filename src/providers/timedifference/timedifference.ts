6c7f574c0d43781fb203abcf4d992bc19ee09712
Miro We <39390507+Wyrda1@users.noreply.github.com>
2018-09-29 12:31:49 +0200

    added all events

--- src/providers/timedifference/timedifference.ts
+++ src/providers/timedifference/timedifference.ts
@@ -14,7 +14,7 @@ export class TimedifferenceProvider {
     console.log('Hello TimedifferenceProvider Provider');
   }
 
- 
+
   public anzeigemonat;
   public anzeigetag;
   public anzeigestunde;
@@ -25,13 +25,40 @@ export class TimedifferenceProvider {
   runTimer(page) {
     console.log("runTimer aufgerufen", page);
     var ophase = moment("2018-10-08T08:00");
-    var kneipenbesuch = moment("2018-10-08T20:00");
-    var fruehstueck1 = moment("2018-10-08T08:00");
-    var fruestueck2 = moment("2018-10-08T08:00");
-    var fruestueck3 = moment("2018-10-08T08:00");
+    var kneipenbesuch = moment("2018-10-08T19:00");
+    var fruehstueck1 = moment("2018-10-09T09:00");
+    var schlogaRallye = moment("2018-10-09T10:30");
+    var karaokeParty = moment("2018-10-09T19:30");
+    var fruestueck2 = moment("2018-10-10T09:00");
+    var elsassFahrt = moment("2018-10-10T16:30");
+    var cenClub = moment("2018-10-10T22:30");
+    var fruestueck3 = moment("2018-10-11T09:00");
+    var diskavery = moment("2018-10-11T11:30");
+    var thursdayNight = moment("2018-10-11T19:00");
+    var profCafe = moment("2018-10-12T09:00");
+    var unirundgang = moment("2018-10-12T11:30");
+    var flunkyBowl = moment("2018-10-12T18:00");
+    var krokoKeller = moment("2018-10-12T23:00");
+    var gahaParty = moment("2018-10-13T19:00");
+
     setTimeout(() => {
       var ophasetogo = moment.duration(moment(ophase).diff(moment())).asSeconds(); //time difference between current time and o-phase in seconds
       var kneipenbesuchtogo = moment.duration(moment(kneipenbesuch).diff(moment())).asSeconds();
+      var fruehstueck1togo = moment.duration(moment(fruehstueck1).diff(moment())).asSeconds();
+      var schlogaRallyetogo = moment.duration(moment(schlogaRallye).diff(moment())).asSeconds();
+      var karaokePartytogo = moment.duration(moment(karaokeParty).diff(moment())).asSeconds();
+      var fruestueck2togo = moment.duration(moment(fruestueck2).diff(moment())).asSeconds();
+      var elsassFahrttogo = moment.duration(moment(elsassFahrt).diff(moment())).asSeconds();
+      var cenClubtogo = moment.duration(moment(cenClub).diff(moment())).asSeconds();
+      var fruestueck3togo = moment.duration(moment(fruestueck3).diff(moment())).asSeconds();
+      var diskaverytogo = moment.duration(moment(diskavery).diff(moment())).asSeconds();
+      var thursdayNighttogo = moment.duration(moment(thursdayNight).diff(moment())).asSeconds();
+      var profCafetogo = moment.duration(moment(profCafe).diff(moment())).asSeconds();
+      var unirundgangtogo = moment.duration(moment(unirundgang).diff(moment())).asSeconds();
+      var flunkyBowltogo= moment.duration(moment(flunkyBowl).diff(moment())).asSeconds();
+      var krokoKellertogo = moment.duration(moment(krokoKeller).diff(moment())).asSeconds();
+      var gahaPartytogo = moment.duration(moment(gahaParty).diff(moment())).asSeconds();
+
       var ms = Math.min(ophasetogo, kneipenbesuchtogo);
       this.anzeigemonat = moment.duration(ms, "seconds").months(); //months of the time difference
       this.anzeigetag = moment.duration(ms, "seconds").days(); //days of the time difference and so on
